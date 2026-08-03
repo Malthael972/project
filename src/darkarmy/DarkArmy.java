fcaee7eff782bda8ca3c1224424b48a89445daee
Malthael972 <frankvaliquette123@gmail.com>
2026-08-02 19:11:52 -0500

    Create DarkArmy.java

---
+++ src/darkarmy/DarkArmy.java
@@ -0,0 +1,13 @@
+package darkarmy;
+
+import mindustry.mod.Mod;
+import mindustry.game.EventType;
+import darkarmy.content.DarkUnits
+
+  public class DarkArmy extend Mod() {
+  @Override
+    public void loadContent() {
+
+  }
+}
+
